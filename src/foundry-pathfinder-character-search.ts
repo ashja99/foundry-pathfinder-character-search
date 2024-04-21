ca40187ea7092d26c7dd404162cea2ab96a5b303
Ashley Ja <musicaddictaja@gmail.com>
2024-04-21 02:23:29 -0400

    i've got a non-functional search box in vaguely the right place

--- src/foundry-pathfinder-character-search.ts
+++ src/foundry-pathfinder-character-search.ts
@@ -1,8 +1,18 @@
-
 console.log('hello world from foundry-pathfinder-character-search.ts still working');
 
-Hooks.on('renderCharacterSheetPF2e', (charactersheet, html, data) => {
-	console.log('character sheet rendering: ',charactersheet);
-	console.log('html data is: ', html[0].innerHTML);
-	console.log('hook data is: ', data.actor);
+const searchButtonHTML =  `
+<div class="pc">
+	<h3>
+		<span class="value"><input type="text" class="search-input" placeholder="Search..."></span>
+			<a><i class="fa-solid fa-fw fa-search"></i></a>
+	</h3>
+</div>
+`;
+
+Hooks.on('renderCharacterSheetPF2e', (_, characterSheet) => {
+	console.log('renderCharacterSheetPF2e hook', characterSheet);
+
+	const characterDetails = characterSheet.find(`section.char-details`);
+	characterDetails.append(searchButtonHTML);
+
 });
